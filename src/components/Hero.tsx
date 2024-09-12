bee4a97a9ba054f1d73f83622c89b983ab9d2128
Picred <andry4you@hotmail.it>
2024-09-12 11:41:21 +0200

    chore: add some template components

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div className="hero bg-transparent min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://pics.craiyon.com/2023-10-03/c64134a721434b048ae228cc6a16643e.webp"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-7xl font-extralight">Andrei Stefan</h1>
+          <p className="py-8 overflow-hidden">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+            doloremque eum nostrum aliquid nihil facere similique veniam
+            ratione, accusamus ex. Eum nostrum quam iusto sapiente eligendi
+            pariatur expedita, qui corrupti, voluptate explicabo eius placeat
+            non illo odit voluptas aspernatur iste numquam accusamus!
+          </p>
+          <button className="btn btn-primary mr-5">Contact Me!</button>
+
+          <button className="btn btn-neutral">Browse Projects</button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Hero } from "../components/Hero";
 
 export const Home = () => {
   return (
     <>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-        <p className="text-center text-5xl text-info h-full "> Homepage </p>
+        <Navbar />
+        <Hero />
       </div>
     </>
   );
