7cd71d34833f78124ed53b9752d3ed50c02284d3
Picred <andry4you@hotmail.it>
2024-09-12 18:33:49 +0200

    feat: navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-7xl font-extralight">Andrei Stefan</h1>
+          <h1 className="text-7xl font-extralight text-clip">Andrei Stefan</h1>
           <p className="py-8 overflow-hidden">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
             doloremque eum nostrum aliquid nihil facere similique veniam
@@ -17,9 +17,13 @@ export const Hero = () => {
             pariatur expedita, qui corrupti, voluptate explicabo eius placeat
             non illo odit voluptas aspernatur iste numquam accusamus!
           </p>
-          <button className="btn btn-primary mr-5">Contact Me!</button>
+          <button className="btn btn-primary mr-5">
+            <p className="text-primary-content">Contact Me!</p>
+          </button>
 
-          <button className="btn btn-neutral">Browse Projects</button>
+          <button className="btn btn-neutral">
+            <p className="text-neutral-content">Browse Projects!</p>
+          </button>
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,58 +2,22 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-transparent">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Projects</a>
-            </li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost text-xl text-white font-sans">AS.</a>
+    <>
+      <p className="absolute text-5xl text-info pl-5 btn btn-ghost overflow-hidden">
+        AS.
+      </p>
+
+      <div role="tablist" className="tabs tabs-bordered mx-96">
+        <p role="tab" className="tab tab-active text-white">
+          Home
+        </p>
+        <p role="tab" className="tab text-white">
+          About
+        </p>
+        <p role="tab" className="tab text-white">
+          Projects
+        </p>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Projects</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
-    </div>
+    </>
   );
 };
