8c102e90b066ec6c21ed924a471b21b9b444169e
Andrei Daniel Stefan <s356980@studenti.polito.it>
2025-12-20 20:13:47 +0100

    feat: snowfall on winter season

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Root } from "./components/Root";
 import "./index.css";
+import { Snowfall } from "react-snowfall";
 
-
+const currentMonth = new Date().getMonth();
+const isWinterSeason = currentMonth === 11 || currentMonth === 0;
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
+        {isWinterSeason && <Snowfall color="#dee4fd" />}
         <Root />
     </StrictMode>
 );
